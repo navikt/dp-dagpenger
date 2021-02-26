d4ff17b206f52c9cd5a0dc0da47f5f410689a6d6
Tonje Mjøvik Myran <tonje.mjovik.myran@nav.no>
2021-02-26 10:07:58 +0100

    Testa ut knapper og diverse
    Co-authored-by: André Roaldseth <andre.roaldseth@nav.no>
    Co-authored-by: Celine Dam <celine.ten.dam@nav.no>

--- next.config.js
+++ next.config.js
@@ -1,41 +1,23 @@
-const withLess = require("@zeit/next-less");
-const withCss = require("@zeit/next-css");
-const withTM = require("next-transpile-modules")(navFrontendModuler());
 const csp = require("./src/.csp");
 
-function navFrontendModuler() {
-  return Object.keys(require("./package.json").dependencies).filter(
-    (it) => it.startsWith("nav-") || it.startsWith("@navikt/")
-  );
-}
-
-module.exports = withCss(
-  withLess(
-    withTM({
-      basePath: "",
-      async headers() {
-        return [
+module.exports = {
+  basePath: "",
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
           {
-            source: "/:path*",
-            headers: [
-              {
-                key: "Content-Security-Policy",
-                value: csp,
-              },
-            ],
+            key: "Content-Security-Policy",
+            value: csp,
           },
-        ];
+        ],
       },
-      i18n: {
-        locales: ["no", "en"],
-        defaultLocale: "no",
-        localeDetection: false,
-      },
-      cssModules: true,
-      cssLoaderOptions: {
-        importLoaders: 1,
-        localIdentName: "[local]___[hash:base64:5]",
-      },
-    })
-  )
-);
+    ];
+  },
+  i18n: {
+    locales: ["no", "en"],
+    defaultLocale: "no",
+    localeDetection: false,
+  },
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,45 @@
-import { Knapp } from "nav-frontend-knapper";
+import { Fareknapp, Hovedknapp, Knapp } from "nav-frontend-knapper";
+import { AlertStripeFeil } from "nav-frontend-alertstriper";
+import Veilederpanel from "nav-frontend-veilederpanel";
 import Layout from "../components/layout";
+import "nav-frontend-knapper-style/dist/main.css";
+import "nav-frontend-alertstriper-style/dist/main.css";
+import "nav-frontend-typografi-style/dist/main.css";
+import "nav-frontend-veilederpanel-style/dist/main.css";
 
 export default function Home() {
   return (
     <Layout>
-      <h1>Hello</h1>
-      <Knapp>ifoo</Knapp>
+      <div>
+        <AlertStripeFeil>Husk å sende inn vedlegg</AlertStripeFeil>
+      </div>
 
-      <div id="var">FBARBA</div>
+      <h1>Hello</h1>
+      <div id="knapp1">
+        <Knapp>ifoo</Knapp>
+      </div>
+      <div>
+        <Hovedknapp>Mine dagpengesaker</Hovedknapp>
+      </div>
+
+      <div>
+        <Fareknapp>Send vedlegg</Fareknapp>
+      </div>
+
+      <ul>
+        <li>Test</li>
+        <li>Test</li>
+        <li>Test</li>
+        <li>Test</li>
+      </ul>
+
+      <Veilederpanel svg={""}>Her veileder vi deg</Veilederpanel>
+
+      <div id="var">Fikk du svar på det du lurte på?</div>
 
       <style jsx>{`
         h1 {
-          color: pink;
+          color: green;
         }
       `}</style>
     </Layout>
