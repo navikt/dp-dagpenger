c30c6af6f74985b6b9e24587d1dd26d718893509
André Roaldseth <andre.roaldseth@nav.no>
2021-03-01 19:25:08 +0100

    Sett opp proxy for websocket

---
+++ src/pages/api/ws.ts
@@ -0,0 +1,14 @@
+import nc from "next-connect";
+import { createProxyMiddleware } from "http-proxy-middleware";
+
+const proxy = createProxyMiddleware({
+  target: process.env.WEBSOCKET_API,
+  changeOrigin: true,
+  ws: true,
+  pathRewrite: {
+    "^/api/ws": "",
+  },
+});
+
+// @ts-ignore
+export default nc().use(proxy);
