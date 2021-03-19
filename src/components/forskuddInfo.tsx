e23f97a1d882e0bdeedaf48518c13209b6188afc
Celine Dam <celine.ten.dam@nav.no>
2021-03-19 13:56:08 +0100

    La til lenker i infoboksen om forskudd på dagpenger
    navikt/dagpenger#719

--- src/components/forskuddInfo.tsx
+++ src/components/forskuddInfo.tsx
@@ -1,12 +1,16 @@
 import AlertStripe from "nav-frontend-alertstriper";
 
+const urlSøkeOmForskudd = "https://www.nav.no/dagpenger/forskudd";
+const urlInfoOmForskudd =
+  "https://www.nav.no/no/person/innhold-til-person-forside/nyttig-a-vite/kampanje-korona/forskudd-pa-dagpenger-for-permitterte-og-arbeidsledige";
+
 export const ForskuddDagpengerInfo = () => {
-
-    const tekst = "På grunn av økt saksbehandlingstid på søknad om dagpenger, er det mulig å søke om forskudd på dagpenger. Forskuddet blir trukket fra fremtidige utbetalinger. Se mer informasjon om forskudd.";
-
-    return (
-        <AlertStripe type="info">
-            {tekst}
-        </AlertStripe>
-    )
-}
+  return (
+    <AlertStripe type="info">
+      På grunn av økt saksbehandlingstid på søknad om dagpenger, er det mulig å{" "}
+      <a href={urlSøkeOmForskudd}> søke om forskudd på dagpenger.</a>
+      Forskuddet blir trukket fra fremtidige utbetalinger.{" "}
+      <a href={urlInfoOmForskudd}>Se mer informasjon om forskudd.</a>
+    </AlertStripe>
+  );
+};
