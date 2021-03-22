710d07904d562281f37edf077cfe547ebeec8c79
Amund Øgar Meisal <amund.ogar.meisal@nav.no>
2021-03-22 09:35:39 +0100

    refactor(forskuddinfo): Bruker Lenke-komponent fra design-lib navikt/dagpenger#719

--- src/components/forskuddInfo.tsx
+++ src/components/forskuddInfo.tsx
@@ -1,16 +1,14 @@
 import AlertStripe from "nav-frontend-alertstriper";
+import Lenke from "nav-frontend-lenker";
 
-const urlSøkeOmForskudd = "https://www.nav.no/dagpenger/forskudd";
+const urlSøkeOmForskudd = "https://www.nav.no/dagpenger/forskudd/";
 const urlInfoOmForskudd =
   "https://www.nav.no/no/person/innhold-til-person-forside/nyttig-a-vite/kampanje-korona/forskudd-pa-dagpenger-for-permitterte-og-arbeidsledige";
 
 export const ForskuddDagpengerInfo = () => {
   return (
     <AlertStripe type="info">
-      På grunn av økt saksbehandlingstid på søknad om dagpenger, er det mulig å{" "}
-      <a href={urlSøkeOmForskudd}> søke om forskudd på dagpenger.</a> Forskuddet
-      blir trukket fra fremtidige utbetalinger.{" "}
-      <a href={urlInfoOmForskudd}>Se mer informasjon om forskudd.</a>
+      På grunn av økt saksbehandlingstid på søknad om dagpenger, er det mulig å <Lenke href={urlSøkeOmForskudd}>søke om forskudd på dagpenger.</Lenke> Forskuddet blir trukket fra fremtidige utbetalinger. <Lenke href={urlInfoOmForskudd}>Se mer informasjon om forskudd.</Lenke>
     </AlertStripe>
   );
 };
