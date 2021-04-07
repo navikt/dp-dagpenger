574d3a486742e2d4fc3cb00ff3ecc1158aa16807
Celine Dam <celine.ten.dam@nav.no>
2021-04-07 11:05:57 +0200

    Sette audience
    Co-authored-by: André Roaldseth <andre.roaldseth@nav.no>
    Co-authored-by: Amund Øgar Meisal <amund.ogar.meisal@nav.no>

--- src/pages/api/debug.ts
+++ src/pages/api/debug.ts
@@ -9,7 +9,10 @@ export default nc()
     const session = await getSession({ req });
     if (!session) return res.status(401).end();
     // @ts-ignore
-    const token = await req.getToken(session.access_token);
+    const token = await req.getToken(
+      session.access_token,
+      "dev-gcp:teamdagpenger:dp-innsyn"
+    );
     const data = await fetch(process.env.INNSYN_API, {
       method: "get",
       headers: { Authorization: `Bearer ${token}` },
