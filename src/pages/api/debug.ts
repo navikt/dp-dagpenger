3e8a7033290676b7304eb2b35c168766277a114a
Celine Dam <celine.ten.dam@nav.no>
2021-04-07 11:00:09 +0200

    Kjøre tokenx middelware
    Co-authored-by: André Roaldseth <andre.roaldseth@nav.no>
    Co-authored-by: Amund Øgar Meisal <amund.ogar.meisal@nav.no>

--- src/pages/api/debug.ts
+++ src/pages/api/debug.ts
@@ -1,15 +1,19 @@
 import { getSession } from "../../auth/hooks/session";
 import { NextApiRequest, NextApiResponse } from "next";
+import nc from "next-connect";
+import { tokenx } from "../../auth/middlewares/tokenx";
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const session = await getSession({ req });
-  if (!session) return res.status(401).end();
-  // @ts-ignore
-  const token = await req.getToken(session.access_token);
-  const data = await fetch(process.env.INNSYN_API, {
-    method: "get",
-    headers: { Authorization: `Bearer ${token}` },
-  }).then((data) => data.json());
+export default nc()
+  .use(tokenx)
+  .get(async (req: NextApiRequest, res: NextApiResponse) => {
+    const session = await getSession({ req });
+    if (!session) return res.status(401).end();
+    // @ts-ignore
+    const token = await req.getToken(session.access_token);
+    const data = await fetch(process.env.INNSYN_API, {
+      method: "get",
+      headers: { Authorization: `Bearer ${token}` },
+    }).then((data) => data.json());
 
-  res.json(data);
-};
+    res.json(data);
+  });
