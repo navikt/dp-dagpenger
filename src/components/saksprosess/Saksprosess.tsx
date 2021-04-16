f2c75957343a1bbf96ca47f8d84a5904b34b9f46
Celine Dam <celine.ten.dam@nav.no>
2021-04-16 14:01:04 +0200

    feat(hendelser): Lagde Hendelse for innvigelse, avslag og mangelbrev. Men disse er ikke i bruk enda fordi vi mangler data på det. Lagde også Hendelse for når vi venter på dokumentasjon: venterPåDokumentasjonHendelse
    feat(sakstype): Ny sakstype for "alle vedlegg mottatt".
    
    refactor(Saksprosess): Tekstendringer

--- src/components/saksprosess/Saksprosess.tsx
+++ src/components/saksprosess/Saksprosess.tsx
@@ -6,6 +6,7 @@ import styles from "./saksprosess.module.css";
 type SaksType =
   | "soknadmottatt"
   | "manglendevedlegg"
+  | "allevedleggmottatt"
   | "behandlingstartet"
   | "mangelbrev"
   | "vedtak";
@@ -28,22 +29,22 @@ const renderSeSoknadOgKvittering = () => (
   <Lenke href="#">Se søknaden og kvittering</Lenke>
 );
 const renderDokumentListeLink = () => (
-  <Lenke href="#">Gå til dokumentliste</Lenke>
+  <Lenke href="#">Les brevet fra NAV</Lenke>
 );
-const renderLastOppVedleggLink = () => (
-  <Lenke href="#">Last opp vedlegg</Lenke>
-);
+const renderVedleggLink = (tittel: string) => <Lenke href="#">{tittel}</Lenke>;
 
 const renderLenkeForSakstype = (sakshendelse: Hendelse) => {
   switch (sakshendelse.sakstype) {
     case "soknadmottatt":
       return renderSeSoknadOgKvittering();
     case "manglendevedlegg":
-      return renderLastOppVedleggLink();
+      return renderVedleggLink("Last opp vedlegg");
+    case "allevedleggmottatt":
+      return renderVedleggLink("Se vedleggene");
     case "mangelbrev":
       return renderDokumentListeLink();
     case "vedtak":
-      return <Lenke href="#">Se vedtak</Lenke>
+      return <Lenke href="#">Les vedtaksbrevet</Lenke>;
     default:
       return null;
   }
@@ -68,39 +69,79 @@ export const SaksProsess = (props: SaksprosessProps) => {
   }, [props.antallVedleggsOppgaver]);
 
   const sisteHendelse = (): Hendelse => {
-    if(props.vedtakErFattet) {
+    //TODO: Her må vi sjekke hvovidt det er innvigelse eller avslag og spesifisere dette i tidslinjen, ikke bare vedtak generelt.
+    if (props.vedtakErFattet) {
       return {
         sakstype: "vedtak",
-        tilstand: "utfort", 
-        tittel: "Søknaden din er ferdigbehandlet og det er fattet et vedtak. For å se vedtaket, følg lenken under"
-      }
+        tilstand: "utfort",
+        tittel:
+          "Søknaden din er ferdigbehandlet og det er fattet et vedtak. For å se vedtaket, følg lenken under",
+      };
     }
-    return { tilstand: "inaktiv", tittel: "Når saken din er ferdig behandlet vil du få et varsel på SMS." }
-  }
+    return {
+      tilstand: "inaktiv",
+      tittel: "Når saken din er ferdig behandlet vil du få et varsel på SMS.",
+    };
+  };
+
+  const venterPåDokumentasjonHendelse = (): Hendelse => {
+    return {
+      tilstand: "inaktiv",
+      tittel: `Når du har sendt inn nødvendig dokumentasjon, vil vi behandle søknaden din så raskt som mulig. Dersom vi mangler noe vil du få beskjed.`,
+    };
+  };
 
   const manglendeVedleggsHendelse = (): Hendelse => {
     return {
       sakstype: "manglendevedlegg",
       tilstand: "hendelse",
-      tittel: `Vi mangler ${props.antallManglendeVedleggsOppgaver} av ${props.antallVedleggsOppgaver} vedlegg for å kunne behandle søknaden din`,
+      tittel: `Vi mangler ${props.antallManglendeVedleggsOppgaver} vedlegg for å kunne behandle søknaden din. Husk at du må sende inn nødvendig dokumentasjon senest 14 dager etter at vi mottok søknaden din. Hvis du ikke rekker fristen, ta kontakt med NAV (nav.no/kontakt)`,
     };
   };
 
   const fullfortVedleggsHendelse = (): Hendelse => {
     return {
+      sakstype: "allevedleggmottatt",
       tilstand: "utfort",
-      tittel: "Du har sendt inn alle påkrevde vedlegg."
-    }
-  }
-  
+      tittel:
+        "Vi har mottatt alle vedleggene du skulle sende inn. Saken din vil bli behandlet så raskt som mulig. Du får beskjed dersom vi trenger mer dokumentasjon",
+    };
+  };
+
+  const mangelbrevHendelse = (): Hendelse => {
+    return {
+      sakstype: "mangelbrev",
+      tilstand: "hendelse",
+      tittel:
+        "Du må sende oss flere opplysninger før vi kan behandle søknaden din.",
+    };
+  };
+
+  const innvigelseHendelse = (): Hendelse => {
+    return {
+      sakstype: "vedtak",
+      tilstand: "utfort",
+      tittel:
+        "Du har fått innvilget dagpenger. Det er viktig at du leser hele vedtaket og kontrollerer at opplysningene er riktige.",
+    };
+  };
+
+  const avslagHendelse = (): Hendelse => {
+    return {
+      sakstype: "vedtak",
+      tilstand: "hendelse",
+      tittel:
+        "Du har fått avslag på søknaden om dagpenger, les vedtaksbrevet for å se hvorfor.",
+    };
+  };
+
   const inkluderVedleggsHendelse = () => {
-    if(props.antallManglendeVedleggsOppgaver > 0) {
+    if (props.antallManglendeVedleggsOppgaver > 0) {
       return manglendeVedleggsHendelse();
     } else if (props.antallManglendeVedleggsOppgaver === 0) {
       return fullfortVedleggsHendelse();
     }
-
-  }
+  };
 
   const generateHendelser = (): Hendelse[] => {
     const h: Hendelse[] = [
@@ -111,8 +152,11 @@ export const SaksProsess = (props: SaksprosessProps) => {
         label: props.tidspunktSoknadMottatt,
       },
     ];
-    if(props.antallVedleggsOppgaver > 0) {
-      h.push(inkluderVedleggsHendelse())
+    if (props.antallVedleggsOppgaver > 0) {
+      h.push(inkluderVedleggsHendelse());
+    }
+    if (props.antallManglendeVedleggsOppgaver > 0) {
+      h.push(venterPåDokumentasjonHendelse());
     }
     h.push(sisteHendelse());
     return h;
