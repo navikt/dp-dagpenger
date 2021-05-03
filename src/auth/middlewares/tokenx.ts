3b2407ab66793d5dfb45eaef496ea5445601fc02
Trong Huu Nguyen <trong.huu.nguyen@nav.no>
2021-05-03 11:14:08 +0200

    fix: tokenx expects token_endpoint as audience

--- src/auth/middlewares/tokenx.ts
+++ src/auth/middlewares/tokenx.ts
@@ -22,7 +22,7 @@ export default async function tokenx(
     const additionalClaims = {
       clientAssertionPayload: {
         nbf: now,
-        aud: tokenXClient.issuer.metadata.issuer
+        aud: tokenXClient.issuer.metadata.token_endpoint
       },
     };
 
