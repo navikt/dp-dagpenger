6ba7bb2f3f417495f213282f04ebe2f0fb7eb265
André Roaldseth <andre.roaldseth@nav.no>
2021-05-03 11:52:52 +0200

    Redirect via loginservice

--- src/auth/handlers/callback.ts
+++ src/auth/handlers/callback.ts
@@ -1,8 +1,12 @@
 import passport from "../middlewares/passport";
 
+const successRedirect = process.env.LOGINSERVICE_URL
+  ? `${process.env.LOGINSERVICE_URL}?redirect=${process.env.SELF_URL}`
+  : "/";
+
 export default function callback(req, res, next) {
   return passport.authenticate("idporten", {
-    successRedirect: "/?success",
+    successRedirect,
     failureRedirect: "/?failure",
   })(req, res, next);
 }
