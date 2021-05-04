e9cc62eb56b97843e0c1f610c10629f79ce84822
André Roaldseth <andre.roaldseth@nav.no>
2021-05-04 10:20:42 +0200

    Ta med mocks inn docker context

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,7 @@ import "../styles/global.css";
 import { Provider } from "../auth/hooks/session.hook";
 
 if (process.env.NEXT_PUBLIC_API_MOCKING === "enabled") {
-  require("../../mocks");
+  require("../mocks");
 }
 
 export default function App({ Component, pageProps }: AppProps) {
