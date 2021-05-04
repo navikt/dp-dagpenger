a07719b98c1e9fe39b122b5ef02ca3ab5196a126
André Roaldseth <andre.roaldseth@nav.no>
2021-05-04 12:40:51 +0200

    Legg på en nav-consumer-id for veilarbproxy

--- src/pages/api/registrering.ts
+++ src/pages/api/registrering.ts
@@ -4,7 +4,12 @@ import { createProxyMiddleware } from "http-proxy-middleware";
 const proxy = createProxyMiddleware({
   target: process.env.VEILARBPROXY_URL,
   changeOrigin: true,
+  onProxyReq,
 });
+
+function onProxyReq(proxyReq) {
+  proxyReq.setHeader("Nav-Consumer-Id", "dp-dagpenger");
+}
 
 // @ts-ignore
 export default nc().use(proxy);
