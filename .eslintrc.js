02b83f07d973c5405fa79dd77cd8693a0c959934
André Roaldseth <andre.roaldseth@nav.no>
2021-05-11 13:43:44 +0200

    Legg til ett unntak på a11y-lint om at anchor uten href er lov inne i Link

--- .eslintrc.js
+++ .eslintrc.js
@@ -34,5 +34,13 @@ module.exports = {
         "ts-ignore": "allow-with-description",
       },
     ],
+    "jsx-a11y/anchor-is-valid": [
+      "error",
+      {
+        components: ["Link"],
+        specialLink: ["hrefLeft", "hrefRight"],
+        aspects: ["invalidHref", "preferButton"],
+      },
+    ],
   },
 };
