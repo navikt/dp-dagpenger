a05fc6007e4f71669aab6ac50154c1c16a7e7850
Amund Øgar Meisal <amund.ogar.meisal@nav.no>
2021-06-29 13:15:46 +0200

    chore(perioder): Refaktorering

--- src/pages/api/arbeidssoker/perioder.ts
+++ src/pages/api/arbeidssoker/perioder.ts
@@ -1,43 +1,50 @@
 import { createProxyMiddleware } from "http-proxy-middleware";
 import { withMiddleware } from "../../../auth/middlewares";
 
-const proxy = createProxyMiddleware({
+const veilarbProxy = createProxyMiddleware({
   target: process.env.VEILARBPROXY_URL,
   changeOrigin: true,
   onProxyReq,
   pathRewrite,
 });
 
+const periodeFormatter = new Intl.DateTimeFormat("no", {
+  year: "numeric",
+  month: "2-digit",
+  day: "2-digit",
+});
+
+const ARBEIDSSOKER_PERIODE_FRA_OG_MED_FRATREKK = 105;
+const trekkFraDato = (dato: Date): Date => {
+  return new Date(
+    new Date().setDate(
+      dato.getDate() - ARBEIDSSOKER_PERIODE_FRA_OG_MED_FRATREKK
+    )
+  );
+};
+
 const tilOgMed = new Date();
-const fraOgMed = trekkFraDato(tilOgMed, 105);
+const fraOgMed = trekkFraDato(tilOgMed);
 
-function trekkFraDato(dato: Date, dager: number): Date {
-  return new Date(new Date().setDate(dato.getDate() - dager));
+function formaterDato(date: Date) {
+  return periodeFormatter.format(date).split(".").reverse().join("-");
 }
 
-function formatDate(date: Date) {
-  const formatter = new Intl.DateTimeFormat("no", {
-    year: "numeric",
-    month: "2-digit",
-    day: "2-digit",
-  });
-  return formatter.format(date).split(".").reverse().join("-");
-}
-
-const appendQueries = (user) => {
+const leggTilQueries = (user) => {
   let query = "?";
   if (user) query += `fnr=${user.fnr}&`;
-  query += `fraOgMed=${formatDate(fraOgMed)}`;
-  query += `&tilOgMed=${formatDate(tilOgMed)}`;
+  query += `fraOgMed=${formaterDato(fraOgMed)}`;
+  query += `&tilOgMed=${formaterDato(tilOgMed)}`;
   return query;
 };
 
 function pathRewrite(path, request) {
-  return path + appendQueries(request.user);
+  return path + leggTilQueries(request.user);
 }
+
 function onProxyReq(proxyReq) {
   proxyReq.setHeader("Nav-Consumer-Id", "dp-dagpenger");
 }
 
 // @ts-ignore:mangler grunn
-export default withMiddleware(proxy);
+export default withMiddleware(veilarbProxy);
