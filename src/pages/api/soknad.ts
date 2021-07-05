fd38212ed72db347b2c3350a2f5cbe924fdae750
André Roaldseth <andre.roaldseth@nav.no>
2021-07-05 12:56:40 +0200

    Lag liste med søknader
    Co-authored-by: Amund Ogar Meisal <amund.ogar.meisal@nav.no>
    Co-authored-by: Celine ten Dam <celine.ten.dam@nav.no>

---
+++ src/pages/api/soknad.ts
@@ -0,0 +1,28 @@
+import { NextApiResponse } from "next";
+import { AuthedNextApiRequest, withMiddleware } from "../../auth/middlewares";
+
+async function getApiData(token: string, endpoint: string): Promise<any> {
+  return await fetch(`${process.env.INNSYN_API}/${endpoint}`, {
+    method: "get",
+    headers: { Authorization: `Bearer ${token}` },
+  }).then((data) => data.json());
+}
+
+export async function hentSøknad(token: string): Promise<any[]> {
+  return await getApiData(token, `soknad`);
+}
+
+export const handleSøknad = async (
+  req: AuthedNextApiRequest,
+  res: NextApiResponse
+) => {
+  const user = req.user;
+  if (!user) return res.status(401).end();
+
+  const audience = `${process.env.NAIS_CLUSTER_NAME}:teamdagpenger:dp-innsyn`;
+  const token = await user.tokenFor(audience);
+
+  res.json(await hentSøknad(token));
+};
+
+export default withMiddleware(handleSøknad);
