911d7bd22692b42bb06ee1d4a5db201aba2e243b
André Roaldseth <andre.roaldseth@nav.no>
2021-12-05 20:34:04 +0100

    Logg headers vi sender mot innsyn.

--- src/lib/api/innsyn.ts
+++ src/lib/api/innsyn.ts
@@ -9,20 +9,26 @@ export async function fetchInnsynAPI(
 
   console.log(`(callId: ${callId}) - Henter ${url} fra innsyn.`);
 
-  return fetch(url, {
+  const options = {
     method: "get",
     headers: {
       Authorization: `Bearer ${await token}`,
       "Nav-Consumer-Id": "dp-dagpenger",
       "Nav-Call-Id": callId,
     },
-  })
+  };
+
+  console.log(`(callId: ${callId} - Bruker options: ${options}`);
+
+  return fetch(url, options)
     .then(async (res) => {
       const contentType = res.headers.get("content-type");
       if (!contentType || !contentType.includes("application/json")) {
-        console.log(res.status, res.statusText, res.headers);
         throw new TypeError(
-          `Fikk ikke JSON fra innsyn. Body: (${await res.text()}).`
+          `Fikk ikke JSON fra innsyn. Body: (${await res.text()}) 
+          ${res.status} 
+          ${res.statusText} 
+          ${res.headers}`
         );
       }
       console.log(`(callId: ${callId}) - Fikk svar fra innsyn`);
