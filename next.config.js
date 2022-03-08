ebfaf9f73d2a391e60b25c3f978779e4376236ee
André Roaldseth <andre.roaldseth@nav.no>
2022-03-08 10:44:57 +0100

    Fjern Sentry for å unngå blockering i deploy

--- next.config.js
+++ next.config.js
@@ -1,42 +1,43 @@
 const csp = require("./src/.csp");
 
 const withPlugins = require("next-compose-plugins");
-const { withSentryConfig } = require("@sentry/nextjs");
+//const { withSentryConfig } = require("@sentry/nextjs");
 const withTM = require("next-transpile-modules")(["@navikt/ds-icons"]);
 
-module.exports = withSentryConfig(
-  withPlugins([withTM], {
-    publicRuntimeConfig: {
-      amplitudeKey: process.env.AMPLITUDE_API_KEY,
-    },
-    basePath: `${process.env.NEXT_PUBLIC_BASE_PATH}`,
-    async headers() {
-      return [
-        {
-          source: "/:path*",
-          headers: [
-            {
-              key: "Content-Security-Policy",
-              value: csp,
-            },
-          ],
-        },
-      ];
-    },
-    async redirects() {
-      return [
-        {
-          source: "/app/tema(.*)",
-          destination: "/",
-          permanent: false,
-        },
-      ];
-    },
-    i18n: {
-      locales: ["no", "en"],
-      defaultLocale: "no",
-      localeDetection: false,
-    },
-  }),
-  { silent: true }
-);
+module.exports = withPlugins([withTM], {
+  //withSentryConfig(
+  publicRuntimeConfig: {
+    amplitudeKey: process.env.AMPLITUDE_API_KEY,
+  },
+  basePath: `${process.env.NEXT_PUBLIC_BASE_PATH}`,
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: csp,
+          },
+        ],
+      },
+    ];
+  },
+  async redirects() {
+    return [
+      {
+        source: "/app/tema(.*)",
+        destination: "/",
+        permanent: false,
+      },
+    ];
+  },
+  i18n: {
+    locales: ["no", "en"],
+    defaultLocale: "no",
+    localeDetection: false,
+  },
+});
+/*,
+{ silent: true }
+);*/
