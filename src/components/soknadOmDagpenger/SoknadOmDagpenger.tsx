ac4055c3ab78148f3b4f58e0067b1ba3a760a26f
Rebecca Gjerstad <rebecca.soraya.gjerstad@nav.no>
2022-11-09 12:55:25 +0100

    DAG-591 Endret litt på oppsett

--- src/components/soknadOmDagpenger/SoknadOmDagpenger.tsx
+++ src/components/soknadOmDagpenger/SoknadOmDagpenger.tsx
@@ -4,15 +4,28 @@ import { Ikon } from "../Ikon";
 import { Seksjon } from "../Seksjon";
 
 export const SoknadOmDagpenger = (dato: string) => {
+  function gaTilSendDokumentasjon() {
+    window.location.href = "";
+  }
+
+  function gaTilSeSoknad() {
+    window.location.href = "";
+  }
+
   return (
     <>
-      <Seksjon tittel="Søknad om dagpenger" iconSvg={<Ikon navn="info" />}>
-        <Undertekst style={{ color: "#6A6A6A" }}>
-          Sendt: {/* <time dateTime={dato}>{localeString}</time>- {avsender} */}
-        </Undertekst>
-        <br />
-        <Knapp>Send dokumentasjon</Knapp>
-        <Flatknapp>Se søknaden</Flatknapp>
+      <Seksjon tittel="Søknad om dagpenger">
+        <div style={{ display: "flex" }}>
+          <Ikon navn="info" />
+
+          <Undertekst style={{ color: "#6A6A6A" }}>
+            Sendt:{" "}
+            {/* <time dateTime={dato}>{localeString}</time>- {avsender} */}
+          </Undertekst>
+          <br />
+          <Knapp onClick={gaTilSendDokumentasjon}>Send dokumentasjon</Knapp>
+          <Flatknapp onClick={gaTilSeSoknad}>Se søknaden</Flatknapp>
+        </div>
       </Seksjon>
     </>
   );
