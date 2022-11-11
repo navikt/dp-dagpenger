d5c0b2093944201b3dd45212d491183801bf39ff
André Roaldseth <andre.roaldseth@nav.no>
2022-11-11 20:03:45 +0100

    Bruk en JWT som kan decodes

--- src/__tests__/api/dokumenter/dokumenter.test.ts
+++ src/__tests__/api/dokumenter/dokumenter.test.ts
@@ -7,7 +7,8 @@ const getSession = _getSession as jest.MockedFunction<typeof _getSession>;
 
 beforeEach(() => {
   getSession.mockResolvedValue({
-    token: "123",
+    token:
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyLCJwaWQiOiIxMjMxMjMxMjMifQ.XdPmoIvLFmgz51eH_05WBNOllgWEtp9kYHkWAHqMwEc",
     apiToken: async () => "access_token",
     expiresIn: 123,
   });
