d803ed315659bd67a6c9117971d083ed3ddca2a1
Rebecca Gjerstad <rebecca.soraya.gjerstad@nav.no>
2022-11-16 11:12:16 +0100

    DAG-591 Legg til styling i FullforteSoknader, ikke ferdig

--- src/components/fullforteSoknader/FullforteSoknader.tsx
+++ src/components/fullforteSoknader/FullforteSoknader.tsx
@@ -1,6 +1,8 @@
+import { Notes } from "@navikt/ds-icons";
 import { Flatknapp, Knapp } from "nav-frontend-knapper";
 import { Undertekst, Undertittel } from "nav-frontend-typografi";
 import { Søknad } from "../../pages/api/soknader";
+import styles from "./FullforteSoknader.module.css";
 
 export const FullforteSoknader = (props: Søknad): JSX.Element => {
   const { tittel, datoInnsendt: dato } = props;
@@ -14,16 +16,22 @@ export const FullforteSoknader = (props: Søknad): JSX.Element => {
   }
 
   return (
-    <li>
-      <div>
-        {/* <Ikon navn={ikon} /> */}
-        <Undertittel>{tittel}</Undertittel>
-        <Undertekst style={{ color: "#6A6A6A" }}>Sendt: {dato}</Undertekst>
-        <Undertekst>{status}</Undertekst>
-        <br />
+    <li className={styles.fullforteSoknader}>
+      <div className={styles.fullforteSoknaderDetaljer}>
+        <Notes className={styles.fullfortIkon} />
+
+        <div>
+          <Undertittel>{tittel}</Undertittel>
+          <Undertekst style={{ color: "#6A6A6A" }}>Sendt: {dato}</Undertekst>
+        </div>
+        <Undertekst className={styles.fullfortStatus}>
+          Fullført søknad
+        </Undertekst>
+      </div>
+      <nav className="navigation-container">
         <Knapp onClick={trykkVenstreKnapp}>En tekst her</Knapp>
         <Flatknapp onClick={trykkHoyreKnapp}>En annen tekst her</Flatknapp>
-      </div>
+      </nav>
     </li>
   );
 };
