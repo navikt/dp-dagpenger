e3147adead17c96ac119828e921d6463dfa4feee
Rebecca Gjerstad <rebecca.soraya.gjerstad@nav.no>
2022-11-16 13:43:01 +0100

    DAG-591 Endre variabler til norsk, ikke ferdig

--- src/util/soknadDato.util.ts
+++ src/util/soknadDato.util.ts
@@ -1,8 +1,8 @@
 import { addWeeks, isBefore } from "date-fns";
 
 export function innenfor12Uker(innsendt: string): boolean {
-  const innsendtDate = new Date(innsendt);
-  const today = new Date();
-  const endDate = addWeeks(innsendtDate, 12);
-  return isBefore(today, endDate);
+  const innsendtDato: Date = new Date(innsendt);
+  const iDag: Date = new Date();
+  const sluttDato: Date = addWeeks(innsendtDato, 12);
+  return isBefore(iDag, sluttDato);
 }
