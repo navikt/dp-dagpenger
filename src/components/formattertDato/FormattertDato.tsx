1c51049940b09dba9bdd5cc83afe744bf7647a47
Rebecca Gjerstad <rebecca.soraya.gjerstad@nav.no>
2022-11-16 13:20:38 +0100

    DAG-591 Legg til formattertDato komponent, ikke ferdig

---
+++ src/components/formattertDato/FormattertDato.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { DateTimeFormatOptions } from "next-intl";
+
+interface IProps {
+  dato: string;
+  short?: boolean;
+}
+
+export function FormattertDato(props: IProps) {
+  const locale = "no-NO";
+
+  const utvalg: DateTimeFormatOptions = {
+    year: "numeric",
+    month: props.short ? "2-digit" : "long",
+    day: props.short ? "2-digit" : "numeric",
+  };
+
+  const formattertDato: string = new Date(props.dato).toLocaleDateString(
+    locale,
+    utvalg
+  );
+
+  return <>{formattertDato}</>;
+}
