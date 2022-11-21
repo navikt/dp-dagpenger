6190ef31bbf3b78876131a6b0ca6961fddc840af
Rebecca Gjerstad <rebecca.soraya.gjerstad@nav.no>
2022-11-21 10:36:37 +0100

    DAG-591 Fikser lenker og knapper, ikke ferdig

--- src/components/fullforteSoknader/FullforteSoknader.tsx
+++ src/components/fullforteSoknader/FullforteSoknader.tsx
@@ -1,21 +1,20 @@
 import { Notes } from "@navikt/ds-icons";
 import { Flatknapp, Knapp } from "nav-frontend-knapper";
 import { Undertekst, Undertittel } from "nav-frontend-typografi";
+import Link from "next/link";
 import { Søknad } from "../../pages/api/soknader";
 import { innenfor12Uker } from "../../util/soknadDato.util";
 import { FormattertDato } from "../formattertDato/FormattertDato";
 import styles from "./FullforteSoknader.module.css";
 
 export const FullforteSoknader = (props: Søknad): JSX.Element => {
-  const { tittel, datoInnsendt: dato } = props;
+  const { tittel, datoInnsendt: dato, endreLenke } = props;
 
   const skalKunneEttersende: boolean = innenfor12Uker(dato);
 
-  function sendDokumentasjon(): void {
-    // TODO: denne fikser vi sammen :)
-    // hvis 12 uker, kunne ettersende
-    // etter 12 uker, stoppe ettersende, sende oss noe, bare en knapp
-  }
+  // TODO: denne fikser vi sammen :)
+  // hvis 12 uker, kunne ettersende
+  // etter 12 uker, stoppe ettersende, sende oss noe, bare en knapp
 
   function seSoknaden(): void {
     // TODO: denne fikser vi sammen :)
@@ -34,7 +33,14 @@ export const FullforteSoknader = (props: Søknad): JSX.Element => {
         </div>
       </div>
       <nav className="navigation-container">
+        <Link href={endreLenke} passHref>
+          <a className="knapp knapp--standard">Send dokumentasjon</a>
+        </Link>
+        {/*
+          husk Se svaret på søknaden i Ferdig behandlet
         <Knapp onClick={sendDokumentasjon}>Send dokumentasjon</Knapp>
+
+        */}
         {skalKunneEttersende && (
           <Flatknapp onClick={seSoknaden}>Se søknaden</Flatknapp>
         )}
--- src/components/paabegynteSoknader/PaabegynteSoknader.tsx
+++ src/components/paabegynteSoknader/PaabegynteSoknader.tsx
@@ -1,6 +1,7 @@
 import { Notes } from "@navikt/ds-icons";
-import { Flatknapp, Knapp } from "nav-frontend-knapper";
+import { Flatknapp } from "nav-frontend-knapper";
 import { Undertekst, Undertittel } from "nav-frontend-typografi";
+import Link from "next/link";
 import { PaabegyntSoknad } from "../../pages/api/paabegynteSoknader";
 import { FormattertDato } from "../formattertDato/FormattertDato";
 import styles from "./PaabegynteSoknader.module.css";
@@ -8,9 +9,7 @@ import styles from "./PaabegynteSoknader.module.css";
 export const PaabegynteSoknader = (props: PaabegyntSoknad): JSX.Element => {
   const { tittel, sistEndret: dato, endreLenke } = props;
 
-  function fortsettPaaSoknaden(): void {
-    window.location.href = endreLenke;
-  }
+  console.log("endreLenke", endreLenke);
 
   function slettSoknaden(): void {
     // TODO: FIKS ALL THE THINGS
@@ -28,7 +27,9 @@ export const PaabegynteSoknader = (props: PaabegyntSoknad): JSX.Element => {
         </div>
       </div>
       <nav className="navigation-container">
-        <Knapp onClick={fortsettPaaSoknaden}>Fortsett på søknaden</Knapp>
+        <Link href={endreLenke} passHref>
+          <a className="knapp knapp--standard">Fortsett på søknaden</a>
+        </Link>
         <Flatknapp onClick={slettSoknaden}>Slett søknaden</Flatknapp>
       </nav>
     </li>
