a03e427509563abae7f85ba1d874acbe25ab3313
Hanne Eilertsen Olsen <hanne.eilertsen.olsen@nav.no>
2022-12-02 13:45:26 +0100

    Tilrettelegger for tomme responser fra datakilden

--- src/components/soknader/Soknader.tsx
+++ src/components/soknader/Soknader.tsx
@@ -20,7 +20,7 @@ export const Soknader = ({
   paabegynteSoknader,
   fullforteSoknader,
 }: Props): JSX.Element => {
-  if (paabegynteSoknader.length === 0 && fullforteSoknader.length === 0) {
+  if (paabegynteSoknader?.length === 0 && fullforteSoknader?.length === 0) {
     return null;
   }
 
