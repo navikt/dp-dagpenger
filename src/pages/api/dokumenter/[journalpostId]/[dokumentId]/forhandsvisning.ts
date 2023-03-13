0f5432c9670aafff334883a796f52a3d0446c117
Geir A. Lund <geir.andre.lund@nav.no>
2023-03-13 09:28:33 +0100

    Fix/dokument nedlastning (#71)
    * Fix: Lar filer 'streames' fra APIet istedet for å bufre opp fila, store filer førte til at appen kræsja.
    Skrur av response limit - ref https://nextjs.org/docs/messages/api-routes-response-size-limit
    
    * Return promise til handler.

--- src/pages/api/dokumenter/[journalpostId]/[dokumentId]/forhandsvisning.ts
+++ src/pages/api/dokumenter/[journalpostId]/[dokumentId]/forhandsvisning.ts
@@ -2,22 +2,22 @@ import { NextApiHandler } from "next";
 import { v4 as uuidv4 } from "uuid";
 import { withSentry } from "@sentry/nextjs";
 import { getSession } from "../../../../../lib/auth.utils";
+import { Stream } from "stream";
 
 const audience = `${process.env.SAF_SELVBETJENING_CLUSTER}:teamdokumenthandtering:${process.env.SAF_SELVBETJENING_SCOPE}`;
 
-type Dokument = {
-  headers: {
-    contentDisposition: string;
-  };
-  blob: Blob;
+export const config = {
+  api: {
+    responseLimit: false,
+  },
 };
 
 async function hentDokument(
   token: string,
   journalpostId: string,
-  dokumentInfoId: string
-): Promise<Dokument> {
-  const callId = uuidv4();
+  dokumentInfoId: string,
+  callId: uuidv4
+): Promise<Response> {
   const endpoint = `${process.env.SAF_SELVBETJENING_INGRESS}/rest/hentdokument/${journalpostId}/${dokumentInfoId}/ARKIV`;
 
   const headers = {
@@ -26,47 +26,40 @@ async function hentDokument(
     "Nav-Consumer-Id": "dp-dagpenger",
   };
 
-  try {
-    console.log(`Henter dokument med call-id: ${callId}`);
-    return await fetch(endpoint, { headers }).then(async (res) => {
-      const headers = {
-        contentDisposition: res.headers.get("Content-Disposition"),
-      };
-
-      return { headers, blob: await res.blob() };
-    });
-  } catch (error) {
-    console.error(`Feil fra SAF med call-id ${callId}: ${error}`);
-    throw error;
-  }
+  return fetch(endpoint, { headers });
 }
 
-export const handleHentDokument: NextApiHandler<Buffer> = async (req, res) => {
+export const handleHentDokument: NextApiHandler<Stream> = async (req, res) => {
   const session = await getSession(req);
   if (!session.token) return res.status(401).end();
 
   const { journalpostId, dokumentId } = req.query;
-
-  try {
-    const { blob: dokument, headers } = await hentDokument(
-      await session.apiToken(audience),
-      <string>journalpostId,
-      <string>dokumentId
-    );
-
-    console.log(`Fått dokument, størrelse: ${dokument.size}`);
-
-    res.setHeader(
-      "Content-Disposition",
-      headers.contentDisposition || "inline"
-    );
-    res.setHeader("Content-Type", dokument.type);
-    const buffer = Buffer.from(new Uint8Array(await dokument.arrayBuffer()));
-
-    return res.send(buffer);
-  } catch (errors) {
-    return res.status(500).send(errors);
-  }
+  const callId = uuidv4();
+  return hentDokument(
+    await session.apiToken(audience),
+    <string>journalpostId,
+    <string>dokumentId,
+    callId
+  )
+    .then(async (dokumentResponse) => {
+      res.setHeader(
+        "Content-Disposition",
+        dokumentResponse.headers.get("Content-Disposition") || "inline"
+      );
+      return dokumentResponse.blob();
+    })
+    .then((blob) => {
+      return new Promise((resolve) => {
+        res.setHeader("Content-Type", blob.type);
+        const stream = blob.stream() as unknown as NodeJS.ReadableStream;
+        stream.pipe(res);
+        stream.on("end", resolve);
+      });
+    })
+    .catch((errors) => {
+      console.error(`Feil fra SAF med call-id ${callId}: ${errors}`);
+      return res.status(500).send(errors);
+    });
 };
 
 export default withSentry(handleHentDokument);
