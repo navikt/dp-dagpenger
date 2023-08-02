5eb9d927cbcb4f56e9eb8724274a955fd0c2ad86
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2023-08-02 09:42:02 +0200

    Tester lint-staged prettier

--- src/sanity/sanity-client.ts
+++ src/sanity/sanity-client.ts
@@ -2,7 +2,6 @@ import sanityClient from "@sanity/client";
 
 export const innsynSanityClient = sanityClient({
   projectId: "rt6o382n",
-
   dataset: "production",
   apiVersion: "2021-06-21",
   useCdn: process.env.NODE_ENV === "production",
