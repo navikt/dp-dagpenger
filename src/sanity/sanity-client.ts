22694682f256c3f0d47bde4c2f0fdbb8a2957302
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2023-08-02 09:38:48 +0200

    Bumper lint-staged, husky

--- src/sanity/sanity-client.ts
+++ src/sanity/sanity-client.ts
@@ -2,6 +2,7 @@ import sanityClient from "@sanity/client";
 
 export const innsynSanityClient = sanityClient({
   projectId: "rt6o382n",
+
   dataset: "production",
   apiVersion: "2021-06-21",
   useCdn: process.env.NODE_ENV === "production",
