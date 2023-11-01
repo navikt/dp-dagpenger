6125e67d6ee478022a3b3417da9f6a13bf2f1a41
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2023-11-01 12:41:50 +0100

    Oppdatere next config

--- next.config.js
+++ next.config.js
@@ -19,45 +19,43 @@ const myAppDirectives = {
     "https://sentry.gc.nav.no/api/86/security/?sentry_key=98d1497555654049a7d46e29a5208e61",
 };
 
-module.exports = withPlugins([withTM], {
-  //withSentryConfig(
-  publicRuntimeConfig: {
-    amplitudeKey: process.env.AMPLITUDE_API_KEY,
-    NEXT_PUBLIC_SOKNADSDIALOG: process.env.NEXT_PUBLIC_SOKNADSDIALOG,
-  },
-  basePath: `${process.env.NEXT_PUBLIC_BASE_PATH}`,
-  async headers() {
-    const csp = await buildCspHeader(myAppDirectives, {
-      env: process.env.DEKORATOR_ENV,
-    });
+module.exports = async (phase) =>
+  withPlugins([withTM], {
+    //withSentryConfig(
+    publicRuntimeConfig: {
+      amplitudeKey: process.env.AMPLITUDE_API_KEY,
+      NEXT_PUBLIC_SOKNADSDIALOG: process.env.NEXT_PUBLIC_SOKNADSDIALOG,
+    },
+    basePath: `${process.env.NEXT_PUBLIC_BASE_PATH}`,
+    async headers() {
+      const csp = await buildCspHeader(myAppDirectives, {
+        env: process.env.DEKORATOR_ENV,
+      });
 
-    return [
-      {
-        source: "/:path*",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: csp,
-          },
-        ],
-      },
-    ];
-  },
-  async redirects() {
-    return [
-      {
-        source: "/app/tema(.*)",
-        destination: "/",
-        permanent: false,
-      },
-    ];
-  },
-  i18n: {
-    locales: ["no", "en"],
-    defaultLocale: "no",
-    localeDetection: false,
-  },
-});
-/*,
-{ silent: true }
-);*/
+      return [
+        {
+          source: "/:path*",
+          headers: [
+            {
+              key: "Content-Security-Policy",
+              value: csp,
+            },
+          ],
+        },
+      ];
+    },
+    async redirects() {
+      return [
+        {
+          source: "/app/tema(.*)",
+          destination: "/",
+          permanent: false,
+        },
+      ];
+    },
+    i18n: {
+      locales: ["no", "en"],
+      defaultLocale: "no",
+      localeDetection: false,
+    },
+  })(phase, { undefined });
