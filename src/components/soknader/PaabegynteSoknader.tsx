7d3858221c8a1e5731a851cb97a4336466d1c263
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2023-12-01 09:46:23 +0100

    Flyttet nav tilbake fra article for paabegyntSoknader

--- src/components/soknader/PaabegynteSoknader.tsx
+++ src/components/soknader/PaabegynteSoknader.tsx
@@ -22,14 +22,14 @@ export function PaabegynteSoknader(props: PaabegyntSoknad) {
         <Tag variant="neutral" size="small" className={styles.soknadTag}>
           {getAppText("paabegynt-soknad.soknad-er-ikke-sendt-inn")}
         </Tag>
-        <nav className={styles.soknadLinksContainer}>
-          <Link href={endreLenke} passHref>
-            <Button as="a" variant="secondary" size="small">
-              {getAppText("paabegynt-soknad.fortsett-paa-soknaden")}
-            </Button>
-          </Link>
-        </nav>
       </article>
+      <nav className={styles.soknadLinksContainer}>
+        <Link href={endreLenke} passHref>
+          <Button as="a" variant="secondary" size="small">
+            {getAppText("paabegynt-soknad.fortsett-paa-soknaden")}
+          </Button>
+        </Link>
+      </nav>
     </li>
   );
 }
