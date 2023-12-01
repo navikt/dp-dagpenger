9df601f3b95ba3ea29e5126ee159447ba79b16cf
Thor Håkon Bredesen <thor.hakon.bredesen@gmail.com>
2023-12-01 12:51:56 +0100

    Fiks TypeScript-feil

--- src/__tests__/api/dokumenter/forhåndsvisning.test.ts
+++ src/__tests__/api/dokumenter/forhåndsvisning.test.ts
@@ -1,9 +1,10 @@
 import { createMocks } from "node-mocks-http";
 import handleHentDokument from "../../../pages/api/dokumenter/[journalpostId]/[dokumentId]/forhandsvisning";
 import { getSession as _getSession } from "../../../lib/auth.utils";
+import type { MockedFunction } from "vitest";
 
 vi.mock("../../../lib/auth.utils");
-const getSession = _getSession as vi.MockedFunction<typeof _getSession>;
+const getSession = _getSession as MockedFunction<typeof _getSession>;
 
 beforeEach(() => {
   getSession.mockResolvedValue({
