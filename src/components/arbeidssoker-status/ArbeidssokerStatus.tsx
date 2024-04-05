42a7c71a362925b8b3bc8b098c82509618013899
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2024-04-05 18:51:23 +0200

    Fjerne logikk for arbeidssøkerstatus, viser feilmelding med en gang

--- src/components/arbeidssoker-status/ArbeidssokerStatus.tsx
+++ src/components/arbeidssoker-status/ArbeidssokerStatus.tsx
@@ -1,40 +1,40 @@
-import useSWR from "swr";
+// import useSWR from "swr";
 import { useSanity } from "../../context/sanity-context";
-import { Alert, Skeleton } from "@navikt/ds-react";
+// import { Alert, Skeleton } from "@navikt/ds-react";
 import { PortableText } from "@portabletext/react";
 import styles from "./ArbeidssokerStatus.module.css";
 
 export function ArbeidssokerStatus() {
-  const { data, error, isLoading } = useSWR(
-    `${process.env.NEXT_PUBLIC_BASE_PATH}/api/arbeidssoker/perioder`,
-  );
+  // const { data, error, isLoading } = useSWR(
+  //   `${process.env.NEXT_PUBLIC_BASE_PATH}/api/arbeidssoker/perioder`,
+  // );
 
-  const registered = data?.findIndex((periode) => periode.avsluttet === null) !== -1;
+  // const registered = data?.findIndex((periode) => periode.avsluttet === null) !== -1;
   const { getRichText } = useSanity();
 
-  if (isLoading) {
-    return (
-      <div style={{ fontSize: "7rem" }}>
-        <Skeleton variant="text" width="100%" />
-      </div>
-    );
-  }
+  // if (isLoading) {
+  //   return (
+  //     <div style={{ fontSize: "7rem" }}>
+  //       <Skeleton variant="text" width="100%" />
+  //     </div>
+  //   );
+  // }
 
-  if (!isLoading && error) {
-    return (
-      <Alert variant="warning" className={styles.arbeidssokerStatusNotRegisteredAlertBox}>
-        <PortableText value={getRichText("arbeidssokers-status.teknisk-feil")} />
-      </Alert>
-    );
-  }
+  // if (!isLoading && error) {
+  //   return (
+  //     <Alert variant="warning" className={styles.arbeidssokerStatusNotRegisteredAlertBox}>
+  //       <PortableText value={getRichText("arbeidssokers-status.teknisk-feil")} />
+  //     </Alert>
+  //   );
+  // }
 
-  if (!isLoading && !error && !registered) {
-    return (
-      <Alert variant="warning" className={styles.arbeidssokerStatusNotRegisteredAlertBox}>
-        <PortableText value={getRichText("arbeidssokers-status.er-ikke-registrert")} />
-      </Alert>
-    );
-  }
+  // if (!isLoading && !error && !registered) {
+  //   return (
+  //     <Alert variant="warning" className={styles.arbeidssokerStatusNotRegisteredAlertBox}>
+  //       <PortableText value={getRichText("arbeidssokers-status.er-ikke-registrert")} />
+  //     </Alert>
+  //   );
+  // }
 
   // Midlertidig feilmeling på grunn av teknisk feil i arbeidssøkerstaatus
   return (
