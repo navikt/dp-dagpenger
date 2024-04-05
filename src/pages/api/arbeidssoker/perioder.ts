88f50bf21903c042093535e05b5631a419c387f2
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2024-04-05 16:28:39 +0200

    Debug arbeidsøker perioder

--- src/pages/api/arbeidssoker/perioder.ts
+++ src/pages/api/arbeidssoker/perioder.ts
@@ -49,6 +49,7 @@ const perioderHandler: NextApiHandler<IArbeidssokerperiode[]> = async (
 
     const perioder = await response.json();
 
+    console.log(`🔥arbeidssøker perioder :`, perioder);
     return res.status(response.status).json(perioder);
   } catch (error) {
     logger.error(
