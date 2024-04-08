e869508c52ff63bd630f2895cce1a61839b47cec
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2024-04-08 10:41:15 +0200

    Fjern midlertidig feilmelding om arbeidsøkkerregistering

--- src/components/arbeidssoker-status/ArbeidssokerStatus.tsx
+++ src/components/arbeidssoker-status/ArbeidssokerStatus.tsx
@@ -1,47 +1,40 @@
-// import useSWR from "swr";
+import useSWR from "swr";
 import { useSanity } from "../../context/sanity-context";
-import { Alert } from "@navikt/ds-react";
+import { Alert, Skeleton } from "@navikt/ds-react";
 import { PortableText } from "@portabletext/react";
 import styles from "./ArbeidssokerStatus.module.css";
 
 export function ArbeidssokerStatus() {
-  // const { data, error, isLoading } = useSWR(
-  //   `${process.env.NEXT_PUBLIC_BASE_PATH}/api/arbeidssoker/perioder`,
-  // );
-
-  // const registered = data?.findIndex((periode) => periode.avsluttet === null) !== -1;
-  const { getRichText } = useSanity();
-
-  // if (isLoading) {
-  //   return (
-  //     <div style={{ fontSize: "7rem" }}>
-  //       <Skeleton variant="text" width="100%" />
-  //     </div>
-  //   );
-  // }
-
-  // Midlertidig feilmeling på grunn av teknisk feil i arbeidssøkerstaatus
-  return (
-    <Alert variant="warning" className={styles.arbeidssokerStatusNotRegisteredAlertBox}>
-      <PortableText value={getRichText("arbeidssokers-status.teknisk-feil")} />
-    </Alert>
+  const { data, error, isLoading } = useSWR(
+    `${process.env.NEXT_PUBLIC_BASE_PATH}/api/arbeidssoker/perioder`,
   );
 
-  // if (!isLoading && error) {
-  //   return (
-  //     <Alert variant="warning" className={styles.arbeidssokerStatusNotRegisteredAlertBox}>
-  //       <PortableText value={getRichText("arbeidssokers-status.teknisk-feil")} />
-  //     </Alert>
-  //   );
-  // }
+  const registered = data?.findIndex((periode) => periode.avsluttet === null) !== -1;
+  const { getRichText } = useSanity();
 
-  // if (!isLoading && !error && !registered) {
-  //   return (
-  //     <Alert variant="warning" className={styles.arbeidssokerStatusNotRegisteredAlertBox}>
-  //       <PortableText value={getRichText("arbeidssokers-status.er-ikke-registrert")} />
-  //     </Alert>
-  //   );
-  // }
+  if (isLoading) {
+    return (
+      <div style={{ fontSize: "7rem" }}>
+        <Skeleton variant="text" width="100%" />
+      </div>
+    );
+  }
+
+  if (!isLoading && error) {
+    return (
+      <Alert variant="warning" className={styles.arbeidssokerStatusNotRegisteredAlertBox}>
+        <PortableText value={getRichText("arbeidssokers-status.teknisk-feil")} />
+      </Alert>
+    );
+  }
+
+  if (!isLoading && !error && !registered) {
+    return (
+      <Alert variant="warning" className={styles.arbeidssokerStatusNotRegisteredAlertBox}>
+        <PortableText value={getRichText("arbeidssokers-status.er-ikke-registrert")} />
+      </Alert>
+    );
+  }
 
   return (
     <div className={styles.arbeidssokerStatusContainer}>
