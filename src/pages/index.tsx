674a59a9b6a18ff4e814c7093f48686cdf3742e1
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2024-05-31 10:56:11 +0200

    Legge til referanse til favicon filer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -97,6 +97,9 @@ export default function Status({ fullforteSoknader, paabegynteSoknader, env }: P
     <>
       <Head>
         <title>{getAppText("meta.tittel")}</title>
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
       </Head>
       <main className="mine-dagpenger-app">
         <PageHero hasFullforteSoknader={fullforteSoknader?.length > 0} />
