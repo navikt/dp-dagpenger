14df585af3fbae8078cea563ccb6400d72395674
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2024-09-13 10:29:40 +0200

    Oppdatere linksGroq og settingsGroq spørring

--- src/sanity/sanity-queries.ts
+++ src/sanity/sanity-queries.ts
@@ -22,19 +22,19 @@ const linkFields = `{
 
 export const appTextsGroq = `* [_type=="mineDagpengerAppText" && language==$baseLang]{
     ...coalesce(* [textId==^.textId && language && $lang][0]${appTextFields}, ${appTextFields})
-  }`;
+}`;
 
 const richTextsGroq = `* [_type=="mineDagpengerRichText" && language==$baseLang]{
   ...coalesce(* [textId==^.textId && language && $lang][0]${infoTextsFields}, ${infoTextsFields})
-  }`;
+}`;
 
 const linksGroq = `* [_type=="mineDagpengerLink" && language==$baseLang]{
-  ...coalesce(* [textId==^.textId && language && $lang][0]${linkFields}, ${linkFields})
-  }`;
+  ...coalesce(* [linkId==^.linkId && language && $lang][0]${linkFields}, ${linkFields})
+}`;
 
 const settingsGroq = `* [_type=="mineDagpengerSetting" && language==$baseLang]{
-  ...coalesce(* [textId==^.textId && language && $lang][0]${settingFields}, ${settingFields})
-  }`;
+  ...coalesce(* [settingId==^.settingId && language && $lang][0]${settingFields}, ${settingFields})
+}`;
 
 export const allTextsQuery = `{
   "appTexts": ${appTextsGroq},
