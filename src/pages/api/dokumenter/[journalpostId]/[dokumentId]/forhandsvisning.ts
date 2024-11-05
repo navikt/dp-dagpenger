53629a318620454848b6195d5a5999887290131e
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2024-11-05 13:45:38 +0100

    Bytter ut buffer med ReadableWebToNodeStream

--- src/pages/api/dokumenter/[journalpostId]/[dokumentId]/forhandsvisning.ts
+++ src/pages/api/dokumenter/[journalpostId]/[dokumentId]/forhandsvisning.ts
@@ -1,5 +1,6 @@
 import { logger } from "@navikt/next-logger";
 import { NextApiHandler } from "next";
+import { ReadableWebToNodeStream } from "readable-web-to-node-stream";
 import { v4 as uuidv4 } from "uuid";
 import { getSession } from "../../../../../lib/auth.utils";
 
@@ -11,59 +12,32 @@ export const config = {
   },
 };
 
-async function hentDokument(
-  token: string,
-  journalpostId: string,
-  dokumentInfoId: string,
-  callId: uuidv4,
-): Promise<Response> {
-  const endpoint = `${process.env.VITE_SAF_SELVBETJENING_INGRESS}/rest/hentdokument/${journalpostId}/${dokumentInfoId}/ARKIV`;
-
-  const headers = {
-    Authorization: `Bearer ${token}`,
-    "Nav-Callid": callId,
-    "Nav-Consumer-Id": "dp-dagpenger",
-  };
-
-  return fetch(endpoint, { headers, cache: "no-store" });
-}
-
-const handleHentDokument: NextApiHandler<Buffer> = async (req, res) => {
+const handleHentDokument: NextApiHandler<ReadableWebToNodeStream> = async (req, res) => {
   const session = await getSession(req);
   if (!session.token) return res.status(401).end();
 
-  const { journalpostId, dokumentId } = req.query;
   const callId = uuidv4();
-  return hentDokument(
-    await session.apiToken(audience),
-    <string>journalpostId,
-    <string>dokumentId,
-    callId,
-  )
-    .then(async (dokumentResponse) => {
-      res.setHeader(
-        "Content-Disposition",
-        dokumentResponse.headers.get("Content-Disposition") || "inline",
-      );
+  const { journalpostId, dokumentId } = req.query;
+  const oboToken = await session.apiToken(audience);
+  const requestUrl = `${process.env.VITE_SAF_SELVBETJENING_INGRESS}/rest/hentdokument/${journalpostId}/${dokumentId}/ARKIV`;
+  const requestHeaders = {
+    headers: {
+      Authorization: `Bearer ${oboToken}`,
+      "Nav-Callid": callId,
+      "Nav-Consumer-Id": "dp-dagpenger",
+    },
+  };
 
-      res.setHeader(
-        "Content-Type",
-        dokumentResponse.headers.get("Content-Type") || "application/octet-stream",
-      );
+  try {
+    const response = await fetch(requestUrl, requestHeaders);
+    const readableWebStream = response.body;
+    const nodeStream = new ReadableWebToNodeStream(readableWebStream);
 
-      // Vi kan ikke bruke ReadableStream direkte fra fetch her, går over til å returnere buffer i stedet.
-      // Vi fant feil med ReadableStream.pipe når vi oppgraderte til Node v18, som har innebygget fetch
-      // (fra web). Før hadde de polyfillet en fetch fra node.
-      // Senere kan vi teste ut https://www.npmjs.com/package/readable-web-to-node-stream
-      const arrayBuffer = await dokumentResponse.arrayBuffer();
-      const buffer = Buffer.from(arrayBuffer);
-
-      return res.status(dokumentResponse.status).send(buffer);
-    })
-    .catch((errors) => {
-      logger.error(`Feil fra SAF med call-id ${callId}: ${errors}`);
-      return res.status(500).send(errors);
-    });
+    res.status(200).send(nodeStream);
+  } catch (errors) {
+    logger.error(`Feil fra SAF med call-id ${callId}: ${errors}`);
+    return res.status(500).send(errors);
+  }
 };
 
 export default handleHentDokument;
